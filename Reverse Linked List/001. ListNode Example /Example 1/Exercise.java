ae2e76cb076f51facdeacb3def04f04344f25356
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-03-29 01:16:47 -0400

    flutter

--- Reverse Linked List/001. ListNode Example /Example 1/Exercise.java
+++ Reverse Linked List/001. ListNode Example /Example 1/Exercise.java
@@ -43,3 +43,36 @@ public class Exercise {
         }
     }
 }
+
+/* Ejercicio LeetCode */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+
+        ListNode current = head;
+        ListNode prev = null;
+
+        while(current != null){
+            
+            ListNode next = current.next; 
+            
+            current.next = prev; 
+            
+            prev = current; 
+            current = next; 
+        }
+
+        return prev; 
+    }
+}
