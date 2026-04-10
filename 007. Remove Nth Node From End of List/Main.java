c7202ae8feffb1b7e773de88e61694917de43702
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-04-09 23:46:46 -0400

    java

--- 007. Remove Nth Node From End of List/Main.java
+++ 007. Remove Nth Node From End of List/Main.java
@@ -1,45 +1,47 @@
-package LeetCode.Remove_Duplicates_from_Sorted_List;
+package LeetCode.Remove_Nth_Node_From_End_of_List;
 
 public class Main {
+
     public static class ListNode{
         int val;
         ListNode next;
 
         public ListNode(int val) {
             this.val = val;
-            this.next = null;
+            this.next =null;
         }
     }
 
     public static void main(String[] args) {
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(4);
-        ListNode n5 = new ListNode(5);
+        ListNode list = new ListNode(1);
+        list.next = new ListNode(2);
+        list.next.next = new ListNode(3);
+        list.next.next.next = new ListNode(4);
+        list.next.next.next.next = new ListNode(5);
 
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-        n4.next = n5;
+        ListNode dummy = new ListNode(0);
+        dummy.next = list;
 
-        ListNode current = n1;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        int n = 2;
 
-        while(current != null && current.next != null){
-            if(current.val == current.next.val){
-                current.next = current.next.next;
-            }else{
-                current = current.next;
-            }
-
+        for(int i = 0; i <= n; i++){
+            fast = fast.next;
         }
 
-        ListNode temp = n1;
-
-        while(temp != null){
-            System.out.print(temp.val + (temp.next != null ? " -> " : ""));
-            temp = temp.next;
+        while(fast.next != null){
+            fast = fast.next;
+            slow = slow.next;
         }
 
+        slow.next = slow.next.next;
+
+        ListNode current = dummy.next;
+
+        while(current != null){
+            System.out.print(current.val + (current.next != null? " -> " :""));
+            current = current.next;
+        }
     }
 }
