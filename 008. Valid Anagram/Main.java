a8fd1f436e13810eddf21e7b4a7a7bd29915ec76
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-04-19 21:28:08 -0400

    java

---
+++ 008. Valid Anagram/Main.java
@@ -0,0 +1,39 @@
+package LeetCode.Valid_anagram;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> contenedor = new HashMap<>();
+
+        for (char letra : s.toCharArray()) {
+            contenedor.put(letra, contenedor.getOrDefault(letra, 0) + 1);
+        }
+
+        for (char letra : t.toCharArray()) {
+            Integer contador = contenedor.get(letra);
+
+            if (contador == null || contador == 0) {
+                return false;
+            }
+
+            contenedor.put(letra, contador - 1);
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        System.out.println(main.isAnagram("anagram", "nagaram")); // true
+        System.out.println(main.isAnagram("rat", "car")); // false
+
+    }
+}
