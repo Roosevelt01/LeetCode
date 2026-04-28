cd17f57f34b5dfe67d5a8a37f3069a8f7c9b84fc
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-04-28 00:01:34 -0400

    java

---
+++ 009. Valid Palindrome/Main.java
@@ -0,0 +1,41 @@
+package LeetCode.Valid_PalIndrome;
+
+public class Main {
+    public boolean isPalindrome(String s){
+        int left = 0;
+        int right = s.length()-1;
+
+        while(left < right){
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            char leftChar = Character.toLowerCase(s.charAt(left));
+            char rightChar = Character.toLowerCase(s.charAt(right));
+
+
+            if(leftChar != rightChar){
+                return false;
+            }
+
+            left++;
+            right--;
+
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Main programa = new Main();
+
+        System.out.println("--- INICIANDO PRUEBA ---");
+        System.out.println(programa.isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
