f54a2bef91500eeb2eea413e974868fdfc7ed861
Roosevelt01 <94496123+Roosevelt01@users.noreply.github.com>
2026-09-22 23:35:07 -0400

    leetcode

---
+++ HashMap_Set/Easy/majorityElement.java
@@ -0,0 +1,31 @@
+package LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> elements = new HashMap<>();
+
+        //P
+        for (int num : nums) {
+            elements.put(num, elements.getOrDefault(num, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : elements.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                return entry.getKey();
+            }
+
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Solution main = new Solution();
+        int [] nums = {3,2,3};
+
+        System.out.println(main.majorityElement(nums));
+    }
+}
